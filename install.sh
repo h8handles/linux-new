6755f63571ee339e773bdb273e787cb9ecd227d9
Garrett <h8handles@gmail.com>
2021-08-01 06:59:03 -0400

    adding wordlists and metaspolit

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@
 
 
 
-sudo apt install gnome-tweaks  pluma terminator flameshot gobuster
+sudo apt install gnome-tweaks  pluma terminator flameshot gobuster nmap 
 
 
 #installing metasploit via curl 
@@ -13,3 +13,12 @@ sudo apt install gnome-tweaks  pluma terminator flameshot gobuster
 curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb > msfinstall && \
   chmod 755 msfinstall && \
   ./msfinstall
+
+
+#adding a directory for wordlists and the seclist
+sudo mkdir /usr/share/wordlists/seclist
+
+sudo git clone https://github.com/danielmiessler/SecLists.git /usr/share/wordlists/seclist
+cd /usr/share/wordlists
+sudo git clone https://github.com/3ndG4me/KaliLists.git
+cd ~/
