31bb12ffd2446c2a622ee46a6c371ff5fca885c2
h8handles <67421443+h8handles@users.noreply.github.com>
2022-02-17 13:07:46 -0500

    added some git hub repos
    adds and installs a couple of tools I always want into opt directory.

--- install.sh
+++ install.sh
@@ -21,4 +21,17 @@ sudo mkdir /usr/share/wordlists/seclist
 sudo git clone https://github.com/danielmiessler/SecLists.git /usr/share/wordlists/seclist
 cd /usr/share/wordlists
 sudo git clone https://github.com/3ndG4me/KaliLists.git
+
+
+#optional tools from github . 
+cd /opt 
+sudo git clone https://github.com/stealthcopter/deepce.git
+sudo git clone https://github.com/carlospolop/PEASS-ng.git
+sudo cp /PEASS-ng/linPEAS/linpeas.sh .
+sudo git clone https://github.com/ffuf/ffuf.git
+cd ffuf ; go get ; go build
+cd ..
+
+
+
 cd ~/
